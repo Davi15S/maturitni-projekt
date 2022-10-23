1942dc1ba570310731bd80ab969ab49141cc0ca2
Davi15S <nguyenxd072@gmail.com>
2022-10-23 14:53:46 +0200

    Grid szstem init

---
+++ Maturitní projekt/Assets/Grid/Scripts/Testing.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils;
+
+public class Testing : MonoBehaviour
+{
+    Grid grid;
+    void Start()
+    {
+        grid = new Grid(4, 3, 10f);
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            grid.SetValue(UtilsClass.GetMouseWorldPosition(Camera.main), 25);
+        }
+    }
+}
---
+++ Maturitní projekt/Assets/SpriteGrid/Scripts/AstarTile.cs
@@ -0,0 +1,8 @@
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New AstarTile", menuName = "Tiles/AstarTile")]
+public class AstarTile : Tile
+{
+
+}
