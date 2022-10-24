b2c49040d42363c402e1fbda23ad69db76d4ed56
Davi15S <nguyenxd072@gmail.com>
2022-10-24 15:09:40 +0200

    Pathfinding

--- Maturitní projekt/Assets/SpriteGrid/Scripts/Astar.cs
+++ Maturitní projekt/Assets/SpriteGrid/Scripts/Astar.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
+using System;
+using System.Linq;
 
 public enum TileType { START, GOAL, WATER, GRASS, PATH }
 
@@ -14,8 +16,12 @@ public class Astar : MonoBehaviour
     [SerializeField] private Tile[] tiles;
     private Vector3Int startPos, goalPos;
     private HashSet<Node> openList;
+    private HashSet<Node> closedList;
+    private Stack<Vector3Int> path;
+    private List<Vector3Int> waterTiles = new List<Vector3Int>();
     private Dictionary<Vector3Int, Node> allNodes = new Dictionary<Vector3Int, Node>();
     private Node current;
+    private int gScore = 10;
 
     void Update()
     {
@@ -42,15 +48,21 @@ public class Astar : MonoBehaviour
     {
         current = GetNode(startPos);
         openList = new HashSet<Node>();
+        closedList = new HashSet<Node>();
         openList.Add(current);
     }
 
     private void Algorithm()
     {
         if (current == null) { Initialize(); }
-        List<Node> neighbors = FindNeighbors(current.Position);
-        ExamineNeighbors(neighbors, current);
-        AstarDebugger.MyInstance.CreateTiles(openList, startPos, goalPos);
+        while (openList.Count > 0 && path == null)
+        {
+            List<Node> neighbors = FindNeighbors(current.Position);
+            ExamineNeighbors(neighbors, current);
+            UpdateCurrentTile(ref current);
+            path = GeneratePath(current);
+        }
+        AstarDebugger.MyInstance.CreateTiles(openList, closedList, allNodes, startPos, goalPos, path);
     }
 
     private List<Node> FindNeighbors(Vector3Int parentPos)
@@ -63,14 +75,13 @@ public class Astar : MonoBehaviour
             {
                 Vector3Int neighborPos = new Vector3Int(parentPos.x - x, parentPos.y - y, parentPos.z);
 
-                if ((x == 0 || y == 0) && neighborPos != startPos && tilemap.GetTile(neighborPos))
+                if ((x == 0 || y == 0) && neighborPos != startPos && tilemap.GetTile(neighborPos) && !waterTiles.Contains(neighborPos))
                 {
                     Node neighbor = GetNode(neighborPos);
                     neighbors.Add(neighbor);
                 }
             }
         }
-
         return neighbors;
     }
 
@@ -78,8 +89,37 @@ public class Astar : MonoBehaviour
     {
         for (int i = 0; i < neighbors.Count; i++)
         {
-            openList.Add(neighbors[i]);
+            Node neighbor = neighbors[i];
+
+            if (openList.Contains(neighbor))
+            {
+                if (current.G + gScore < neighbor.G)
+                {
+                    CalcValues(current, neighbor, gScore);
+                }
+            }
+            else if (!closedList.Contains(neighbor))
+            {
+                CalcValues(current, neighbor, gScore);
+                openList.Add(neighbor);
+            }
         }
+    }
+
+    private void CalcValues(Node parent, Node neighbor, int cost)
+    {
+        neighbor.Parent = parent;
+        neighbor.G = parent.G + cost;
+        neighbor.H = ((Math.Abs(neighbor.Position.x - goalPos.x) + Math.Abs(neighbor.Position.y - goalPos.y)) * 10);
+        neighbor.F = neighbor.G + neighbor.H;
+    }
+
+    private void UpdateCurrentTile(ref Node current)
+    {
+        openList.Remove(current);
+        closedList.Add(current);
+
+        if (openList.Count > 0) { current = openList.OrderBy(x => x.F).First(); }
     }
 
     private Node GetNode(Vector3Int pos)
@@ -102,6 +142,26 @@ public class Astar : MonoBehaviour
     {
         if (tileType == TileType.START) { startPos = clickPos; }
         else if (tileType == TileType.GOAL) { goalPos = clickPos; }
+        else if (tileType == TileType.WATER)
+        {
+            tilemap.SetTile(clickPos, tiles[(int)tileType]);
+            waterTiles.Add(clickPos);
+        }
         tilemap.SetTile(clickPos, tiles[(int)tileType]);
+    }
+
+    private Stack<Vector3Int> GeneratePath(Node current)
+    {
+        if (current.Position == goalPos)
+        {
+            Stack<Vector3Int> finalPath = new Stack<Vector3Int>();
+            while (current.Position != startPos)
+            {
+                finalPath.Push(current.Position);
+                current = current.Parent;
+            }
+            return finalPath;
+        }
+        return null;
     }
 }
--- Maturitní projekt/Assets/SpriteGrid/Scripts/AstarDebugger.cs
+++ Maturitní projekt/Assets/SpriteGrid/Scripts/AstarDebugger.cs
@@ -19,15 +19,46 @@ public class AstarDebugger : MonoBehaviour
     [SerializeField] private GameObject debugTextPref;
     private List<GameObject> debugObjects = new List<GameObject>();
 
-    public void CreateTiles(HashSet<Node> openList, Vector3Int start, Vector3Int goal)
+    public void CreateTiles(HashSet<Node> openList, HashSet<Node> closedList, Dictionary<Vector3Int, Node> allNodes, Vector3Int start, Vector3Int goal, Stack<Vector3Int> path = null)
     {
-        foreach (Node node in openList)
+        foreach (Node node in openList) { ColorTile(node.Position, openColor); }
+        foreach (Node node in closedList) { ColorTile(node.Position, closedColor); }
+
+        if (path != null)
         {
-            ColorTile(node.Position, openColor);
+            foreach (Vector3Int pos in path)
+            {
+                if (pos != start && pos != goal)
+                {
+                    ColorTile(pos, pathColor);
+                }
+            }
         }
 
         ColorTile(start, startColor);
         ColorTile(goal, goalColor);
+
+        foreach (KeyValuePair<Vector3Int, Node> node in allNodes)
+        {
+            if (node.Value.Parent != null)
+            {
+                GameObject go = Instantiate(debugTextPref, canvas.transform);
+                go.transform.position = grid.CellToWorld(node.Key);
+                debugObjects.Add(go);
+                GenerateDebugText(node.Value, go.GetComponent<DebugText>());
+            }
+        }
+    }
+
+    private void GenerateDebugText(Node node, DebugText debugText)
+    {
+        debugText.P.text = $"P:{node.Position.x}, {node.Position.y}";
+        debugText.F.text = $"F:{node.F}";
+        debugText.G.text = $"G:{node.G}";
+        debugText.H.text = $"H:{node.H}";
+        if (node.Parent.Position.x < node.Position.x && node.Position.y == node.Parent.Position.y) { debugText.MyArrow.localRotation = Quaternion.Euler(new Vector3(0, 0, 180)); }
+        else if (node.Parent.Position.x == node.Position.x && node.Position.y < node.Parent.Position.y) { debugText.MyArrow.localRotation = Quaternion.Euler(new Vector3(0, 0, 90)); }
+        else if (node.Parent.Position.x == node.Position.x && node.Position.y > node.Parent.Position.y) { debugText.MyArrow.localRotation = Quaternion.Euler(new Vector3(0, 0, -90)); }
     }
 
     public void ColorTile(Vector3Int pos, Color color)
