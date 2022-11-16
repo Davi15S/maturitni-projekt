5a8f4e66b32991b7996edded4252d95a3489fc12
Davi15S <nguyenxd072@gmail.com>
2022-11-16 14:00:58 +0100

    Pause menu

---
+++ Maturitní projekt/Assets/Game/Menu/Scripts/FloorScript.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FloorScript : MonoBehaviour
+{
+    public static int floor = 0;
+}
--- Maturitní projekt/Assets/Game/Menu/Scripts/MainMenu.cs
+++ Maturitní projekt/Assets/Game/Menu/Scripts/MainMenu.cs
@@ -5,6 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
-    public void PlayGame() {SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);}
-    public void Quit() {Application.Quit();}
+    public void PlayGame() { SceneManager.LoadScene("Game"); }
+    public void Quit() { Application.Quit(); }
 }
