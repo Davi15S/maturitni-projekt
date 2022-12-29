c14c83e81cf0cca66f5c14dc367e3e1c0a88ecae
Davi15S <nguyenxd072@gmail.com>
2022-12-29 15:58:29 +0100

    Testing commiting to branch

--- Maturitní projekt/Assets/Tests/Math/Scripts/Cable.cs
+++ Maturitní projekt/Assets/Tests/Math/Scripts/Cable.cs
@@ -27,6 +27,7 @@ public class Cable : MonoBehaviour
         mySprite = GetComponent<SpriteRenderer>();
         tileMapScript = GetComponentInParent<TilemapScript>();
         tilemap = GetComponentInParent<Tilemap>();
+        // Testing committing to main branch
 
         if (!isRobotics)
         {
