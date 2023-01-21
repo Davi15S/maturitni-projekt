bb1573938589ef50e710458ae39fba5ad1c895d3
Davi15S <nguyenxd072@gmail.com>
2023-01-21 18:12:46 +0100

    Task for network

---
+++ Maturitní projekt/Assets/Tests/Network/Scripts/ResultInput.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ResultInput : MonoBehaviour
+{
+    private TMP_InputField inputField;
+
+    private void Start()
+    {
+        inputField = GetComponent<TMP_InputField>();
+        inputField.onValueChanged.AddListener(delegate { ValueChangeCheck(); });
+    }
+    public void ValueChangeCheck()
+    {
+        NetworkManager.instance.CheckResult(inputField.text);
+    }
+}
